017e79c4ab6c809fc61a462fccba6fede34ec69a
Akhil Anil <akhil.karun@gmail.com>
2015-12-22 14:51:10 +0530

    Optimizations
    Code clean-up and optimizationz

--- src/DisplayImage.java
+++ src/DisplayImage.java
@@ -1,4 +1,3 @@
-
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GraphicsDevice;
@@ -22,54 +21,71 @@ import javax.swing.JPanel;
 public class DisplayImage extends JPanel {
 
 	private static final String IMAGE = "/Users/akhilkarun/Desktop/download.jpeg";
-	static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-	static int width = gd.getDisplayMode().getWidth();
-	static int height = gd.getDisplayMode().getHeight();
-	
+	static GraphicsDevice gd = GraphicsEnvironment
+			.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
 	public static void main(String avg[]) throws IOException {
-		
-		System.out.println(width);
-		System.out.println(height);
+
 		DisplayImage abc = new DisplayImage();
 	}
-	
 
-	    public DisplayImage()  throws IOException {
-	    	GridLayout gridLayout = new GridLayout(2,2);
-	    	BufferedImage img = ImageIO.read(new File(IMAGE)); 
-	    	ImageIcon icon = new ImageIcon(ImageResize.resizeImage(img, height/2, width/2));
-	    	ImageIcon icon1 = new ImageIcon(ImageResize.resizeImage(img, height/2, width/2));
-	    	ImageIcon icon2 = new ImageIcon(ImageResize.resizeImage(img, height/2, width/2));
-	         JLabel label1 = new JLabel(icon); 
-	         JLabel label2 = new JLabel(icon1); 
-	         JLabel label3 = new JLabel(icon2);
-	         JLabel label4 = new JLabel(icon2); 
+	public DisplayImage() throws IOException {
+		GridLayout gridLayout = new GridLayout(2, 2);
+		BufferedImage img = ImageIO.read(new File(IMAGE));
+		
+		ImageIcon icon = new ImageIcon(ImageResize.resizeImageWithHint(img,
+				getImageHeight(), getImageWidth()));
+		ImageIcon icon1 = new ImageIcon(ImageResize.resizeImage(img,
+				getImageHeight(), getImageWidth()));
+		ImageIcon icon2 = new ImageIcon(ImageResize.resizeImage(img,
+				getImageHeight(), getImageWidth()));
+		JLabel label1 = new JLabel(icon1);
+		JLabel label2 = new JLabel(icon);
+		JLabel label3 = new JLabel(icon);
+		JLabel label4 = new JLabel(icon2);
 
-	         add(label1);
-	         add(label2);
-	         add(label3);
-	         JFrame frame = new JFrame();
-	 		frame.setLayout(gridLayout);
-	 		frame.setState(Frame.MAXIMIZED_BOTH);
-	 		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-	 		frame.add(label1);
-	 		frame.add(label2);
-	 		frame.add(label3);
-	 		frame.add(label4);
-	 		frame.setVisible(true);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 		
-	    }
-//	public DisplayImage1() throws IOException {
-//		BufferedImage img = ImageIO.read(new File(IMAGE));
-//		ImageIcon icon = new ImageIcon(img);
-//		JFrame frame = new JFrame();
-//		frame.setLayout(new FlowLayout());
-//		frame.setState(Frame.NORMAL);
-//		JLabel lbl = new JLabel();
-//		lbl.setIcon(icon);
-//		frame.add(lbl);
-//		frame.setVisible(true);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//	}
+		add(label1);
+		add(label2);
+		add(label3);
+		JFrame frame = new JFrame();
+		frame.setLayout(gridLayout);
+		frame.setState(Frame.MAXIMIZED_BOTH);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+		frame.add(label1);
+		frame.add(label2);
+		frame.add(label3);
+		frame.add(label4);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+	private static int getImageWidth() {
+		return getScreenWidth() / 2;
+	}
+
+	private static int getScreenWidth() {
+		return gd.getDisplayMode().getWidth();
+	}
+
+	private static int getImageHeight() {
+		return getScreenHeight() / 2;
+	}
+
+	private static int getScreenHeight() {
+		return gd.getDisplayMode().getHeight();
+	}
+
+	// public DisplayImage1() throws IOException {
+	// BufferedImage img = ImageIO.read(new File(IMAGE));
+	// ImageIcon icon = new ImageIcon(img);
+	// JFrame frame = new JFrame();
+	// frame.setLayout(new FlowLayout());
+	// frame.setState(Frame.NORMAL);
+	// JLabel lbl = new JLabel();
+	// lbl.setIcon(icon);
+	// frame.add(lbl);
+	// frame.setVisible(true);
+	// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// }
 }
--- src/ImageResize.java
+++ src/ImageResize.java
@@ -55,6 +55,22 @@ public class ImageResize {
 		g.dispose();
 		return resizedImage;
 	}
+	
+	public static BufferedImage resizeImageWithHint(BufferedImage originalImage, int height,int width){
+
+		BufferedImage resizedImage = new BufferedImage(width, height, 5);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, width, height, null);
+		g.dispose();
+		g.setComposite(AlphaComposite.Src);
+
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		return resizedImage;
+	}
+	
 	private static BufferedImage resizeImage(BufferedImage originalImage, int type) {
 		BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
 		Graphics2D g = resizedImage.createGraphics();
